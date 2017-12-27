4e444d6d4a83387f254f329b5b2070e42695fc62
mtizim <mtizim1@gmail.com>
2017-12-28 00:40:30 +0100

    moving slowly or slowly moving

--- whichlang.py
+++ whichlang.py
@@ -1,5 +1,5 @@
 #import tensorflow as tf
-
+import numpy as np
 #load files
 #working directory needs to be the same as the txts
 
@@ -9,6 +9,17 @@ with open("en.txt") as io:
 	english_words=io.read().split("\n")
 
 
+def word_to_vec(word):
+	#gets word and spits out a 15 by 26 tf vector
+	vec_np=np.zeros((15,26))
+	alphabet="abcdefghijklmnopqrstuvwxyz"
+	#fuck itertools lol
+	i=0
+	for letter in word:
+		vec_np[i][alphabet.find(letter)]=1
+		i+=1
+	vec_tf=tf.convert_to_tensor(vec_np, np.float32)
+	return vec_tf
 #give tf alternating and random words from the dictionaries
 #pooling to check for patterns
 
